de4edbd4093e2006cb0c8da2dac31251ec92a235
Sergio Daniel Lewczuk <sergio.lewczuk@gmail.com>
2021-09-13 21:01:22 -0300

    Init

---
+++ src/test/java/com/proyectoventa/proyectoventa/ProyectoVentaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectoventa.proyectoventa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoVentaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
